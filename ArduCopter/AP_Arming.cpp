9220b349783191ed555fc81804f4a0d716d57b6a
Will <wvalentine@txtav.com>
2023-10-20 09:35:45 -0500

    added first build

--- ArduCopter/AP_Arming.cpp
+++ ArduCopter/AP_Arming.cpp
@@ -7,6 +7,7 @@
 
 bool AP_Arming_Copter::pre_arm_checks(bool display_failure)
 {
+    
     const bool passed = run_pre_arm_checks(display_failure);
     set_pre_arm_check(passed);
     return passed;
